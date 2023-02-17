6f583d6d1181bf86b66f3223ffc49daf1f750aa4
IvanKovalov <kovalevvana8@gmail.com>
2023-02-17 18:10:17 +0300

    Add y postion movement

--- Assets/Scripts/Player/PlayerEntity.cs
+++ Assets/Scripts/Player/PlayerEntity.cs
@@ -7,27 +7,65 @@ namespace Player
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerEntity : MonoBehaviour 
 {
+
+    [Header("HorizontalMovement")]
     [SerializeField] private float _horizontalSpeed;
-    private Rigidbody2D _rigidbody;
     [SerializeField] private bool _faceRight;
+
+    [Header("verticalMovement")]
+    [SerializeField] private float _veticalSpeed;
+    [SerializeField] private float _minSize;
+    [SerializeField] private float _maxSize;
+    [SerializeField] private float _maxVerticalPosition;
+    [SerializeField] private float _minVerticalPosition;
+
+    private Rigidbody2D _rigidbody;
+    private float _sizeModificator;
+
+
+ 
     // Start is called before the first frame update
     private void Start()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
-        
-
+        float positionDifference = _maxVerticalPosition - _minVerticalPosition;
+        float sizeDifference = _maxSize - _minSize;
+        _sizeModificator = sizeDifference / positionDifference;
+        UpdateSize();
     }
 
 
     public void MoveHorizontally(float direction) {
+        SetDirrection(direction);
         Vector2 velocity = _rigidbody.velocity;
         velocity.x = direction * _horizontalSpeed;
         _rigidbody.velocity = velocity;
     }
 
+    public void MoveVertically(float direction) {
+        Vector2 velocity = _rigidbody.velocity;
+        velocity.y = direction * _veticalSpeed;
+        _rigidbody.velocity = velocity;
+
+         if (direction == 0) {
+            return;
+        } 
+
+        float verticalPosition = Mathf.Clamp(transform.position.y, _minVerticalPosition, _maxVerticalPosition);
+        _rigidbody.position = new Vector2(_rigidbody.position.x, verticalPosition);
+        UpdateSize();
+
+    }
+
+    private void UpdateSize () {
+        float verticalDelta = _maxVerticalPosition - transform.position.y;
+        float currentSizeModificator = _minSize + _sizeModificator * verticalDelta;
+        transform.localScale = Vector2.one * currentSizeModificator;
+    }
+
     private void SetDirrection (float direction) {
         if ((_faceRight && direction < 0) || (!_faceRight && direction > 0)) {
-
+            Flip();
         }
     }
 
