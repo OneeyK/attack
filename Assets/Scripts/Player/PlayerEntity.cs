3670c01fb7daa2331b3a199f8adadb8b4859a683
IvanKovalov <kovalevvana8@gmail.com>
2023-02-17 17:21:37 +0300

    Add fliping face

--- Assets/Scripts/Player/PlayerEntity.cs
+++ Assets/Scripts/Player/PlayerEntity.cs
@@ -9,6 +9,7 @@ public class PlayerEntity : MonoBehaviour
 {
     [SerializeField] private float _horizontalSpeed;
     private Rigidbody2D _rigidbody;
+    [SerializeField] private bool _faceRight;
     // Start is called before the first frame update
     private void Start()
     {
@@ -18,10 +19,21 @@ public class PlayerEntity : MonoBehaviour
     }
 
 
-    public void MoveHorizontally (float direction) {
+    public void MoveHorizontally(float direction) {
         Vector2 velocity = _rigidbody.velocity;
         velocity.x = direction * _horizontalSpeed;
         _rigidbody.velocity = velocity;
+    }
+
+    private void SetDirrection (float direction) {
+        if ((_faceRight && direction < 0) || (!_faceRight && direction > 0)) {
+
+        }
+    }
+
+    private void Flip() {
+        transform.Rotate(0, 180, 0);
+        _faceRight = !_faceRight;
     }
 }
 
